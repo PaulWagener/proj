f7fe484520e75175b308fae99448bbd5dc76be50
Paul Wagener <mail@paulwagener.nl>
2023-08-13 09:47:01 +0200

    Fix Y

--- src/proj.rs
+++ src/proj.rs
@@ -1326,7 +1326,7 @@ mod test {
         let t = proj.convert((3968419.4, 379068.0, 4962142.2)).unwrap();
 
         assert_relative_eq!(t.x(), 159783.64355817687);
-        assert_relative_eq!(t.y(), 380108.089631567);
+        assert_relative_eq!(t.y(), 380007.7906745551);
         assert_relative_eq!(t.z(), -27.239349158361257);
     }
 
